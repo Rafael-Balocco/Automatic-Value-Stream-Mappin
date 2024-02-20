4908a1df22ad4a74313d8ea4b4954497bf617ce2
Rafael <rlbalocco0@gmail.com>
2024-02-20 15:54:19 -0700

    .

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ addProcessButton.addEventListener("click", function() {
     const newProcess = document.createElement("div");
     newProcess.classList.add("tab");
     newProcess.innerHTML = `
-        <h2>Supplier</h2>
         <label for="enterpriseName">Supplier Name:</label>
         <input type="text" id="enterpriseName" name="enterpriseName" required>
         <br>
         <label for="creatorName">What it Supplies:</label>
         <input type="text" id="creatorName" name="creatorName" required>
+        <br>
+        <button id="addProcessButton">Add another Supplier</button>
     `;
-
-    // Optionally, add functionality to clear existing input values in the new process section.
 
     const parentElement = document.querySelector(".tab:last-child"); // Get the last existing process section
     parentElement.parentNode.insertBefore(newProcess, parentElement.nextSibling); // Insert the new section after the last one
